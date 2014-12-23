e00170d29f454bbc11e320a17eebb468b52a83a0
bettehem <chris.mustola@gmail.com>
2014-12-23 21:31:04 +0200

    yhteystiedot.java tiedoston päivitystä. Lisätty nappeihin tekstit ja idt. Luotu yhteys javakoodin puolelta

--- app/src/main/java/com/bettehem/virkiaalppifreestyle/Yhteystiedot.java
+++ app/src/main/java/com/bettehem/virkiaalppifreestyle/Yhteystiedot.java
@@ -1,16 +1,22 @@
 package com.bettehem.virkiaalppifreestyle;
 
 import android.app.*;
+import android.content.Intent;
 import android.os.*;
 import android.view.*;
 import android.content.res.*;
+import android.widget.Button;
 
-public class Yhteystiedot extends Activity
+public class Yhteystiedot extends Activity implements View.OnClickListener
 {
-	@Override
+    Button kaleviPuhelu, kaleviViesti, kaleviSposti, ariPuhelu, ariViesti, ariSposti;
+    Intent emailIntent;
+    String yhteystieto, aihe, viesti;
+
+
+    @Override
 	public void onCreate(Bundle savedInstanceState)
 	{
-		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.yhteystiedot);
 
@@ -21,5 +27,74 @@ public class Yhteystiedot extends Activity
 		}else{
 			view.setBackgroundResource(R.drawable.tausta_pysty);
 		}
+
+        muuttujat();
 	}
+
+
+    public void muuttujat(){
+        kaleviPuhelu = (Button) findViewById(R.id.kaleviPuhelu);
+        kaleviViesti = (Button) findViewById(R.id.kaleviViesti);
+        kaleviSposti = (Button) findViewById(R.id.kaleviSposti);
+
+        kaleviPuhelu.setOnClickListener(this);
+        kaleviViesti.setOnClickListener(this);
+        kaleviSposti.setOnClickListener(this);
+
+        ariPuhelu = (Button) findViewById(R.id.ariPuhelu);
+        ariViesti = (Button) findViewById(R.id.ariViesti);
+        ariSposti = (Button) findViewById(R.id.ariSposti);
+
+        ariPuhelu.setOnClickListener(this);
+        ariViesti.setOnClickListener(this);
+        ariSposti.setOnClickListener(this);
+
+        aihe = getString(R.string.app_name);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.kaleviPuhelu:
+
+                break;
+            case R.id.kaleviViesti:
+
+                break;
+
+            case R.id.kaleviSposti:
+                yhteystieto = getString(R.string.kalevi_sposti);
+                break;
+
+            case R.id.ariPuhelu:
+
+                break;
+            case R.id.ariViesti:
+
+                break;
+
+            case R.id.ariSposti:
+                yhteystieto = getString(R.string.ari_sposti);
+                emailmessage();
+                break;
+        }
+    }
+
+    public void sending(){
+
+        String emailaddress[] = {yhteystieto};
+        emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, emailaddress);
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, aihe);
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, viesti);
+        startActivity(emailIntent);
+        finish();
+    }
+
+    public void emailmessage(){
+        viesti = "";
+        sending();
+    }
 }
