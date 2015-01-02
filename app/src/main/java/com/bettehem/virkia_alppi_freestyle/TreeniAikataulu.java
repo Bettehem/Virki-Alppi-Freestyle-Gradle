38a56811828c4f175923c4cd448935c711fa7b3a
bettehem <Chris.mustola@gmail.com>
2015-01-02 20:24:57 +0200

    lisätty näyttöjä treeniaikataulua varten, ja yhteistyökumppaneita varten. Yhteistyökumppaneiden sivu tulee todennäköisemmin valmiiksi ennen treeniaikataulua.

---
+++ app/src/main/java/com/bettehem/virkia_alppi_freestyle/TreeniAikataulu.java
@@ -0,0 +1,16 @@
+package com.bettehem.virkia_alppi_freestyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chris on 2.1.2015.
+ */
+public class TreeniAikataulu extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.treeniaikataulu);
+    }
+}
